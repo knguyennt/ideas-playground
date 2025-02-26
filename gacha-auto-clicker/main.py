891748286438dcd52df9a126786d4eb2216f349b
nguyennguyentruong <nguyenntk.18@grad.uit.edu.vn>
2025-02-27 00:24:29 +0700

    feat: implement hunt demo workflow

--- gacha-auto-clicker/main.py
+++ gacha-auto-clicker/main.py
@@ -1,7 +1,29 @@
 import pyautogui
+import time
 
-a = pyautogui.position()
-pyautogui.click(a)
-print("a: ", a)
-# screenWidth, screenHeight = pyautogui.size()
-pyautogui.click("test.png")
+battle_button="battle.png"
+
+battle_button_loc = pyautogui.locateOnScreen(battle_button, confidence=0.7)
+
+pyautogui.click(battle_button_loc)
+pyautogui.click(battle_button_loc)
+time.sleep(3)
+hunt_stage = "hunt.png"
+hunt_stage_loc = pyautogui.locateOnScreen(hunt_stage, confidence=0.7)
+pyautogui.click(hunt_stage_loc)
+time.sleep(3)
+wyren_hunt = "wyren.png"
+wyren_loc = pyautogui.locateOnScreen(wyren_hunt, confidence=0.7)
+pyautogui.click(wyren_loc)
+time.sleep(3)
+select_team = "select_team.png"
+select_team_loc = pyautogui.locateOnScreen(select_team, confidence=0.7)
+pyautogui.click(select_team_loc)
+time.sleep(3)
+auto_pet = "empty_pet_auto.png"
+auto_pet_loc = pyautogui.locateOnScreen(auto_pet, confidence=0.7)
+pyautogui.click(auto_pet_loc)
+time.sleep(3)
+start_hunt = "start_hunt.png"
+start_hunt_loc = pyautogui.locateOnScreen(start_hunt, confidence=0.7)
+pyautogui.click(start_hunt_loc)
